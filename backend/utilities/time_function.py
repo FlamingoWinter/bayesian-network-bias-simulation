2c429b94ad2b4fb1543019fbeae99af5dc1edba5
david <davidmac1239@gmail.com>
2025-01-07 21:37:31 +0000

    Add new time_function utility

---
+++ backend/utilities/time_function.py
@@ -0,0 +1,18 @@
+import time
+
+
+def time_function(pipeline_stage: str):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print(f"{pipeline_stage}...")
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"{pipeline_stage} completed in {elapsed_time:.4f} seconds.")
+            print("")
+            return result
+
+        return wrapper
+
+    return decorator
