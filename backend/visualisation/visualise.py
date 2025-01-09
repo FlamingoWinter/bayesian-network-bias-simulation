a78a5105edc8e383abbc5e175b8f02a9b3b6f189
david <davidmac1239@gmail.com>
2025-01-09 13:49:15 +0000

    Correct visualise.py to not always use asia filename

--- backend/visualisation/visualise.py
+++ backend/visualisation/visualise.py
@@ -2,4 +2,6 @@ import pymc as pm
 
 
 def visualise_model_as_network(model: pm.Model):
-    pm.model_to_graphviz(model).render("asia", format="png", view=True, cleanup=True)
+    if model.name == "":
+        model.name = "unnamed_network"
+    pm.model_to_graphviz(model).render(model.name, format="png", view=True, cleanup=True)
