f8f8cfdba608dcd93b774468e0325b282d736a3f
david <davidmac1239@gmail.com>
2025-01-10 18:45:41 +0000

    Adjust button styling and override primary color of theme

--- frontend/bias-sim/src/components/ButtonBelowDistribution.svelte
+++ frontend/bias-sim/src/components/ButtonBelowDistribution.svelte
@@ -7,7 +7,7 @@
 
 <button
 	type="button"
-	class="btn btn-sm variant-ghost w-full px-2 pt-0 pb-0.5 text-xs font-oswald font-light leading-0"
+	class="btn btn-sm variant-glass-primary w-full px-2 pt-0 pb-0.5 text-xs font-oswald font-light leading-0"
 	on:mousedown|stopPropagation
 	on:click|stopPropagation
 	on:click={callback}
