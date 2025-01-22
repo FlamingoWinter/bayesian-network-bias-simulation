cd4df1bca18ea0211ab6fd025b19557cf3924212
david <davidmac1239@gmail.com>
2025-01-22 10:58:39 +0000

    Adjust forces on simulation

--- frontend/bias-sim/src/animation/forceSimulation.ts
+++ frontend/bias-sim/src/animation/forceSimulation.ts
@@ -8,7 +8,7 @@ export function applyForceSimulation(graph: Graph,
 																		 linkGroup: SVGGElement,
 																		 markerGroup: SVGDefsElement) {
 	function leftRightForce(alpha: number) {
-		const strength = 30;
+		const strength = 3 * graph.nodes.length;
 
 		graph.links.forEach((link: Link) => {
 			const sourceNode = link.source as Node;
@@ -23,9 +23,9 @@ export function applyForceSimulation(graph: Graph,
 	}
 
 	const simulation = d3.forceSimulation(graph.nodes)
-		.force('link', d3.forceLink(graph.links).id((d: any) => d.id).distance(200).strength(0.25))
-		.force('charge', d3.forceManyBody().strength(-800))
-		.force('center', d3.forceCenter(width / 2, height / 2).strength(1000))
+		.force('link', d3.forceLink(graph.links).id((d: any) => d.id).distance(15 * graph.nodes.length).strength(0.01 * graph.nodes.length))
+		.force('charge', d3.forceManyBody().strength(-360 * graph.nodes.length))
+		.force('center', d3.forceCenter(width / 2, height / 2).strength(120 * graph.nodes.length))
 		.force('left-right', leftRightForce);
 
 	simulation
