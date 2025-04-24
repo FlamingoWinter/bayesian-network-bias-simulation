8f0c44d0abb9579d4eb893c3d507a99d4e39f487
david <davidmac1239@gmail.com>
2025-04-24 17:51:06 +0100

    allow api.modelling-bias.com as host

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "django-insecure-2ow%e3c@kt&x8&c$u0d3xi$27h!-2jh8enooj=_=4arry@+&s_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "api.modelling-bias.com"
+]
 
 # Application definition
 
